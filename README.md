13b84dc30d26e7333e48b80e7699ae5c0ee8a31c
Michal Krombholz <krombholz@gmail.com>
2018-09-03 15:55:39 -0700

    PIR sensor monitoring

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 # Linux-based Picture Frames - "YAPF" (Yet Another Picture Frame)
 
 Converting old laptops and LCD TVs into picture frames.
-The frames are operating under schedule and have motion detector for shut down when nobody is there to see.
 
-See:
-http://minkbot.blogspot.com/2018/07/40-picture-frame-for-under-100.html
-http://minkbot.blogspot.com/2018/08/picture-frame-rpipf-on-schedule.html
-http://minkbot.blogspot.com/2010/12/yapf-yet-another-pricture-frame.html
+The frames are operating under a schedule and have a motion detector to shut down the screen for when nobody is there to see.
+
+Blog:
+
+[http://minkbot.blogspot.com/2018/07/40-picture-frame-for-under-100.html]
+[http://minkbot.blogspot.com/2018/08/picture-frame-rpipf-on-schedule.html]
+[http://minkbot.blogspot.com/2010/12/yapf-yet-another-pricture-frame.html]
+
 
 There are 3 versions of the YAPF:
-+ V1 - ca. '95 laptop (still running)
-+ V2 - ca. '99 laptop (defunk)
-+ V3 - 43" LCD TV + RPI
+
++ V1 - ca. '95 laptop (still running) - TinyCore
++ V2 - ca. '99 laptop (defunk) - TinyCore
++ V3 - 43" LCD TV + RPI - Debian
 
 
--- v3/bin/pf.sh
+++ v3/bin/pf.sh
@@ -1,20 +1,56 @@
+#!/usr/bin/env bash
+
+# Picture Frame control script
+
 logger "PF started"
 
+# last time a user motion was detected (via PIR connected to GPIO18)
+lastmotion=`date +%s`
+
+# loop forever
 while true; do
-  now=`date +%H%M`
+
+  # check for allowed time of day
+  timeok=false
+  tnow=`date +%H%M` # time in HHMM 24h used as single number
+  if [ $tnow -gt 700 ] && [ $tnow -lt 2300 ]; then
+    timeok=true
+  fi
+
+  # check for viewer activity (PIR on GPIO18)
+  nowts=`date +%s` # timestamp in seconds since epoch
+  if [ `gpiord 18` -eq 1 ]; then
+    lastmotion=$nowts
+  fi
+  let lastmotionage=$nowts-$lastmotion
+
+  # decide if ok to show: 10min motion age (600s) and within the right time of day
+  show=false
+  if [ $lastmotionage -lt 600 ] && [ $timeok ]; then
+    show=true
+  fi
+
+  echo $lastmotion $lastmotionage $timeok $show
+
+  # start showing or shutdown the slide show
   fehpid=`pidof feh`
-  if [ $now -gt 700 ] && [ $now -lt 2300 ]; then
-    if [ -z $fehpid ]; then
-      logger "PF now=$now fehpid=$fehpid ON"
+  if $show; then
+    if [ -z $fehpid ]; then # if feh is not running
+      echo "ON"
+      logger "PF ON"
       tvon
       gofeh &
     fi
   else
-    if [ ! -z $fehpid ]; then
-      logger "PF now=$now fehpid=$fehpid OFF"
+    if [ ! -z $fehpid ]; then # if feh is running
+      echo "off"
+      logger "PF OFF"
       skill feh
       tvoff
     fi
   fi
-  sleep 1m 
+
+  # wait a moment before checking again
+  sleep 1
+
 done
