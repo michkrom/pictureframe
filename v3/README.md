80acdc5f73eec813e0f6fa0aae6c4b71af0d9adb
Michal Krombholz <krombholz@gmail.com>
2018-09-26 18:48:27 -0700

    PIR sensor now shuts down TV when nobody is moving around

--- v3/README.md
+++ v3/README.md
@@ -2,7 +2,7 @@
 
 This is 3rd version of YAPF (Yet Another Picture Frame) experiments. 
 
-The files here contain a set of shell scripts and configuration files to turn any Raspberry Pi into a picture frame controller. In fact, these would work with any Debian-based linux with exeption of interracting with PIR sensor via GPIO pins, which is specific to RPi.
+The repository contains a set of shell scripts and configuration files to turn any Raspberry Pi into a picture frame controller. In fact, these would work with any Debian-based linux with exeption of interracting with PIR sensor via GPIO pins, which is specific to RPi.
 
 See:
 [http://minkbot.blogspot.com/2018/07/40-picture-frame-for-under-100.html]
@@ -18,6 +18,7 @@ See:
   + tvon/tvoff - commands to turn the TV on and off via CEC over HDMI
   + gofeh - starts the slideshow via feh program
   + aptu/apti - shortcuts to invoke apt-get conviniently: to install a package or to perform full update
-  + gpiord - read a state of GPIO pin (RPi specific)These
+  + gpiord - read a state of GPIO pin (RPi specific)
+  + checkpin - continously reads the pin and outputs it (used for PIR testing)
 
 The content of this repository can be directly copied into /home/pi  (ie home directory of user 'pi').
--- v3/bin/pf.sh
+++ v3/bin/pf.sh
@@ -6,7 +6,7 @@ logger "PF started"
 
 # last time a user motion was detected (via PIR connected to GPIO18)
 lastmotion=`date +%s`
-
+prevpinstate=0
 # loop forever
 while true; do
 
@@ -19,36 +19,53 @@ while true; do
 
   # check for viewer activity (PIR on GPIO18)
   nowts=`date +%s` # timestamp in seconds since epoch
-  if [ `gpiord 18` -eq 1 ]; then
-    lastmotion=$nowts
+  pinstate=`gpiord 18`
+
+  if [ $pinstate -ne $prevpinstate ]; then
+      #logger "PF motion pin change to $pinstate"
+      if [ $pinstate -eq 1 ]; then 
+         logger "PF motion detected"
+      fi
+      prevpinstate=$pinstate
   fi
+
+  if [ $pinstate -eq 1 ]; then
+    lastmotion=$nowts
+  fi   
   let lastmotionage=$nowts-$lastmotion
+  
+  # logger $nowts $pinstate $lastmotion  $lastmotionage
 
   # decide if ok to show: 10min motion age (600s) and within the right time of day
   show=false
-  if [ $lastmotionage -lt 600 ] && [ $timeok ]; then
+
+  if [ $lastmotionage -lt 600 ] && [ "$timeok" = true ]; then
+  #if [ "$timeok" = true ]; then
     show=true
   fi
 
-  echo $lastmotion $lastmotionage $timeok $show
-
   # start showing or shutdown the slide show
   fehpid=`pidof feh`
-  if $show; then
+  if "$show" = true; then
     if [ -z $fehpid ]; then # if feh is not running
       echo "ON"
       logger "PF ON"
+      #logger "PF $lastmotion : $lastmotionage $timeok $show $fehpid"
       tvon
       gofeh &
     fi
   else
     if [ ! -z $fehpid ]; then # if feh is running
       echo "off"
-      logger "PF OFF"
+      logger "PF off"
+      logger $lastmotion ':' $lastmotionage $timeok $show $fehpid
       skill feh
       tvoff
+      sleep 10 # to work around bouncing of the PIR sencor
     fi
   fi
+
+  echo $lastmotion $lastmotionage $timeok $show $fehpid
 
   # wait a moment before checking again
   sleep 1
